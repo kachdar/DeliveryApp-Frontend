e44795d5489a480a2f7dd4f06b2fe0e34c337124
Daria <kachynska.daria@lll.kpi.ua>
2023-05-28 00:38:55 +0300

    add placed order page

---
+++ src/pages/PlacedOrder/PlacedOrder.jsx
@@ -0,0 +1,16 @@
+import { FaCheckCircle } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import styles from './PlacedOrder.module.css';
+
+const PlacedOrder = () => {
+  return (
+    <div className={styles.result}>
+      <FaCheckCircle className={styles.icon} />
+      <h3>Order placed successfully</h3>
+      <p>To search and view your order, follow the link below </p>
+      <Link to="/">Search Orders</Link>
+    </div>
+  );
+};
+
+export default PlacedOrder;
