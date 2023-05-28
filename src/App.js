4c29e5069d7bbf73dc3309d46ddab1dc4ec2282c
Daria <kachynska.daria@lll.kpi.ua>
2023-05-28 06:46:28 +0300

    push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import { HashRouter, Routes, Route } from 'react-router-dom';
-
 import ShopPage from './pages/ShopPage/ShopPage';
 import CartPage from './pages/CartPage/CartPage';
 import ErrorPage from './pages/ErrorPage/ErrorPage';
 import PlacedOrder from './pages/PlacedOrder/PlacedOrder';
 import Header from './components/Header/Header';
+import { RouterProvider, createHashRouter } from 'react-router-dom';
+
+const router = createHashRouter([
+  {
+    path: '/',
+    element: <Header />,
+    children: [
+      {
+        path: 'checkout',
+        element: <CartPage />,
+      },
+      {
+        path: 'success',
+        element: <PlacedOrder />,
+      },
+    ],
+  },
+]);
 
 function App() {
   return (
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<Header />}>
-          <Route index element={<ShopPage />} />
-          <Route path="/checkout" element={<CartPage />} />
-          <Route path="/success" element={<PlacedOrder />} />
-          <Route path="*" element={<ErrorPage />} />
-        </Route>
-      </Routes>
-    </HashRouter>
+    <RouterProvider router={router} />
+    // <Routes>
+    //   <Route path="/" element={<Header />}>
+    //     <Route index element={<ShopPage />} />
+    //     <Route path="/checkout" element={<CartPage />} />
+    //     <Route path="/success" element={<PlacedOrder />} />
+    //     <Route path="*" element={<ErrorPage />} />
+    //   </Route>
+    // </Routes>
   );
 }
 
